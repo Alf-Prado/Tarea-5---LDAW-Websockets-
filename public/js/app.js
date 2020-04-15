6f67ad76f080e2d1f6de39d10711eec1e80a12d0
Alf-Prado <alfpradoc@gmail.com>
2020-04-14 20:48:46 -0500

    Lógica de Juego

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,38 @@
+window.socket = null;
+var numJugador;
+
+//Conexión con el servidor
+let server = window.location.protocol + "//" + window.location.host;
+window.socket = io.connect(server);
+
+window.socket.on('toast', function(data){
+    showToast(data.message);
+});
+
+//Indicar con que letra se va a jugar
+window.socket.on('iniciarJuego', function(data) {
+    letter = data.letter;
+    numJugador = data.number;
+    console.log("La letra sera: " + letter);
+    document.getElementById('msg').innerHTML = letter;
+});
+
+//Oponente desconectado
+window.socket.on('opponentGone', function() {
+    document.getElementById('msg').innerHTML = "El oponente se ha ido de la partida.";
+    console.log("El oponente se ha desconectado");
+});
+
+//Iniciar contador de basta
+window.socket.on('startCount', function() {
+    showCount();    
+});
+
+
+function messageToServer(msg) {
+    window.socket.emit('message-to-server', {message: msg});
+}
+
 function showToast(msg) {
     console.log("El mensaje es: " + msg);
     $.toast({
@@ -6,21 +41,40 @@ function showToast(msg) {
     })
 }
 
-window.socket = null;
-function connectToSocketIo() 
-{
-    let server = window.location.protocol + "//" + window.location.host;
-    window.socket = io.connect(server);
+function declareBasta(){
+    window.socket.emit('activarBasta');
+}
 
-    window.socket.on('toast', function(data){
-        showToast(data.message);
+function showCount() {
+    var i = 2;
+    var time = setInterval(function() {
+        showToast(i);
+        i--;
+        if(i < 0){
+            clearInterval(time);
+            document.getElementById('msg').innerHTML = "¡Basta!";
+            sendAnswers();
+        }
+    }, 1000);
+}
+
+function sendAnswers(){
+    nombre = document.getElementById('nombre').value;
+    color = document.getElementById('color').value;
+    fruto = document.getElementById('fruto').value;
+
+    window.socket.emit('answers-to-server', {
+        nombre: nombre, 
+        color: color,
+        fruto: fruto,
+        jugador: numJugador
     });
 }
-
-function messageToServer(msg) {
-    window.socket.emit('message-to-server', {message: msg});
-}
-
-$(function() {
-    connectToSocketIo();
+window.socket.on('showResults', function(data) {
+    status = data.status;
+    puntaje = data.puntaje;
+    oponente = data.oponente;
+    
+    console.log(status + " Tu puntaje fue de " + puntaje + " y el de tu oponente fue de " + oponente);
 });
+
--- server.js
+++ server.js
@@ -54,14 +54,298 @@ app.use('/', express.static(__dirname + '/public'));
 // Routes
 app.use('/', webRoutes);
 
+
+//Clase de juego
+class Game {
+
+  constructor() {
+      this.players = [];
+      this.waitList = [];
+      this.randomLetter = null;
+      this.hasFinished = false;
+  }
+
+  //Agregar jugadores
+  addPlayer(player) {
+    //Si no hay dos jugaores activos, se manda el jugador a la lista de activos
+    if (this.players.length < 2) {
+      this.players.push(player);
+      //Si tenemos dos jugadores activos se indica cual es su oponente
+      if(this.players.length == 2) {
+        this.players[0].opponent = this.players[1];
+        this.players[1].opponent = this.players[0];
+        console.log("Opponent of player " + this.players[0].id + " is " + this.players[0].opponent.id);
+        console.log("Opponent of player " + this.players[1].id + " is " + this.players[1].opponent.id);
+      }
+    }
+    //Si ya hay dos jugadores activos se envía al jugador a la lista de espera
+    else
+    {
+      this.waitList.push(player);
+    }
+    
+  }
+
+  //Terminar el juego
+  finishGame() {
+    //Sacamos a los jugadores activos de la lista
+    for(var i = 1; i >= 0; i--) {
+      this.players[i].opponent = 'unmatched';
+      this.players[i].points = 0;
+      this.players[i].status = 'undefined';
+      this.players.pop();
+    }
+    console.log("Active have been eliminated " + this.players.length);
+
+    this.hasFinished = false;
+    //Checamos la lista de espera
+    this.checkWaitList();
+  }
+
+  //Checamos la lista de espera
+  checkWaitList() {
+    var limit = this.waitList.length;
+    for(var i = 0; i < limit; i++) {
+      //Si no hay dos jugadores activos, se manda al primer jugador de la lista de espera a la lista de jugadores activos
+      if (this.players.length < 2) {
+        this.addPlayer(this.waitList[0]);
+        this.waitList.splice(0, 1);
+        console.log("New active player: " + this.players.length);
+        console.log("Updated waitlist: " + this.waitList.length);
+      }
+    }
+  }
+
+  //Ver cuantos jugadores hay en cada lista
+  showPlayers() {
+    console.log("Active players: " + this.players.length);
+    console.log("Wait List: " + this.waitList.length);
+  }
+
+  //Eliminamos a un jugador a partir de su id de una lista
+  deletePlayer(id) {
+    for(var i = 0; i < this.players.length; i++) {
+      if(this.players[i].id == id) {
+        this.players.splice(i, 1);
+        console.log("Active player deleted " + this.players.length);
+        this.checkWaitList();
+        return;
+      }
+    }
+
+    for(var i = 0; i < this.waitList.length; i++) {
+      if(this.waitList[i].id == id) {
+        this.waitList.splice(i, 1);
+        console.log("Wait list player deleted " + this.waitList.length);
+        return;
+      }
+    }
+  }
+
+  //Definimos con que letra se va a jugar
+  calculateLetter(){
+    var letters = 'ABCDEFGHIJKLMNOPQRSTUVWYXZ';
+    this.randomLetter = letters.charAt(Math.floor(Math.random() * letters.length));
+    console.log(this.randomLetter);
+    return this.randomLetter;
+  }
+
+  //Se evaluan las respuestas del usuario
+  //Si empiezan con la letra escogido, se obtienen 10 puntos
+  evalAnswers(nombre, color, fruto, numPlayer){
+    var nombrePoints = 0;
+    var colorPoints = 0;
+    var frutoPoints = 0;
+    var totalPoints;
+
+    if (nombre.charAt(0) == this.randomLetter) {
+      nombrePoints = 10;
+    }
+
+    if (color.charAt(0) == this.randomLetter) {
+      colorPoints = 10;
+    }
+
+    if (fruto.charAt(0) == this.randomLetter) {
+      frutoPoints = 10;
+    }
+
+    totalPoints = nombrePoints + colorPoints + frutoPoints;
+    this.players[numPlayer].points = totalPoints;
+    console.log(this.players[numPlayer].points);
+    
+  }
+
+  findWinner(){
+    console.log("Buscando al ganador");
+
+    if(this.players[0].points > this.players[1].points){
+      this.players[0].status = "¡Ganaste, felicidades!";
+      this.players[1].status = "Perdiste, más suerte para la próxima";
+    }
+    
+    if(this.players[0].points < this.players[1].points){
+      this.players[1].status = "¡Ganaste, felicidades!";
+      this.players[0].status = "Perdiste, más suerte para la próxima";
+    }
+    
+    if (this.players[0].points == this.players[1].points) {
+      this.players[0].status = "¡Empate!";
+      this.players[1].status = "¡Empate!";    
+    }
+
+    this.hasFinished = true;
+
+  }
+}
+
+//Clase del jugador
+class Player {
+
+  constructor(socket) {
+      this.socket = socket;
+      this.id = socket.id;
+      this.opponent = 'unmatched';
+      this.points = 0;
+      this.status = 'undefined';
+  }
+
+  //Se indica el oponente del jugador
+  defineOpponent(player) {
+    this.opponent = player;
+  }
+
+}
+
+//Conexión de jugadores
+let game = new Game();
+var cont = 0;
+
 io.on('connection', (socket) => {
-  console.log("Client connected");
-  socket.emit('toast', {message: "Conectado con el servidor."});
+
+  console.log("Client connected: " + socket.id);
+  player = new Player(socket);
+
+  game.addPlayer(player);  
+
+  socket.emit('toast', {message: `Bienvenido al juego jugador ${player.id}.`});
 
   socket.on('message-to-server', (data) => {
     console.log('message-received', data);
   });
+
+  //Desconexión de un jugador
+  socket.on("disconnect", () => {
+    console.log("Client disconnected: ", socket.id);
+    socket.broadcast.emit("clientdisconnect", socket.id);
+  });
+
+  //Informar al oponente del jugador activo que se desconecto
+  socket.on("disconnect", function() {
+    if(game.players.length == 2){
+      if(game.players[0].socket == socket && game.players[0].opponent != null) {
+        game.players[0].opponent.socket.emit("opponentGone");
+        console.log("El oponente de 0 se ha ido");
+      }
+      else if(game.players[1].socket == socket && game.players[1].opponent != null) {
+        game.players[1].opponent.socket.emit("opponentGone");
+        console.log("El oponente de 1 se ha ido");
+      }
+    }
+
+    game.deletePlayer(socket.id);
+
+  });
+
+  //Si hay dos jugadores el juego puede comenzar
+  if (game.players.length == 2) {
+    var letter = game.calculateLetter();
+    game.players[0].socket.emit("iniciarJuego", {
+      letter: letter,
+      number: 0
+    });
+
+    game.players[1].socket.emit("iniciarJuego", {
+      letter: letter,
+      number: 1
+    });
+  }
+
+  //Indicar que se activo el basta
+  socket.on("activarBasta", function() {
+    console.log("Se activo el basta");
+    game.players[0].socket.emit("startCount");
+    game.players[1].socket.emit("startCount");
+  });
+
+  //Recibir respuestas del cliente
+  socket.on('answers-to-server', (data) => {
+    console.log('Respuestas del jugador: ' + data.jugador + ' es: ', data);
+    game.evalAnswers(data.nombre, data.color, data.fruto, data.jugador);
+    cont++;
+    if (cont == 2) {
+      game.findWinner();
+
+      //Mandarle los resultados al jugador
+      if (game.hasFinished == true) {
+        console.log("GAME OVER");
+        for(i = 0; i < game.players.length; i++){
+          game.players[i].socket.emit("showResults", {
+            status: game.players[i].status,
+            puntaje: game.players[i].points,
+            oponente: game.players[i].opponent.points
+          });
+        }
+
+        //Terminar el juego
+        game.finishGame();
+        cont = 0;
+
+        //Iniciar un nuevo juego
+        if (game.players.length == 2) {
+          var letter = game.calculateLetter();
+          game.players[0].socket.emit("iniciarJuego", {
+            letter: letter,
+            number: 0
+          });
+
+          game.players[1].socket.emit("iniciarJuego", {
+            letter: letter,
+            number: 1
+          });
+        }
+      } 
+    }
+
+  });
+
+  if(game.players.length == 2){  
+    //Checar si ya se tienen las dos respuestas
+    /* if(game.players[0].points != -1 && game.players[1].points != -1){
+      console.log("Finding winner");
+      game.findWinner();
+
+      //Mandarle los resultados al jugador
+      if (game.hasFinished == true) {
+        console.log("GAME OVER");
+        for(i = 0; i < game.players.length; i++){
+          game.players[i].socket.emit("showResults", {
+            status: game.players[i].status,
+            puntaje: game.players[i].points,
+            oponente: game.players[i].opponent.points
+          });
+        }
+
+        //Terminar el juego
+        game.finishGame();
+      } 
+    }*/
+  }
+
+  game.showPlayers();
 })
+
+
 
 // App init
 server.listen(appConfig.expressPort, () => {
